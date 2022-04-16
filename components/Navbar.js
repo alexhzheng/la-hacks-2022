66cc66d32317676c3cf2534ec955bd096b41bab9
Justin <justinkim707@gmail.com>
2022-04-16 14:30:53 -0700

    bjkl

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,10 +3,10 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <navbar className=" flex items-center justify-between max-w-full mt-4 px-24">
-      <h1 className="text-2xl">Split-A-Bill</h1>
+    <navbar className=" flex items-center justify-between max-w-full mt-4 px-24 font-barlow font-medium">
+      <h1 className="text-4xl">Split-A-Bill</h1>
       <button
-        className="bg-sky-400 px-4 py-2 rounded-3xl hover:scale-95 transition duration-150 ease-in-out"
+        className="bg-sky-400 px-4 py-2 rounded-3xl hover:scale-95 transition duration-150 ease-in-out text-xl"
         type="button"
       >
         Connect Wallet
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        barlow: ["Barlow Condensed", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
