cfb57e63aaf522876f42e65193d43b488d590539
Alex <alex.h.zheng@gmail.com>
2022-04-16 16:56:45 -0400

    navbar fucking up spacing

---
+++ components/Navbar.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <navbar className="flex items-center justify-between max-w-full mt-4 px-24">
+      <h1 className="text-2xl">Split-A-Bill</h1>
+      <button
+        className="bg-sky-400 px-4 py-2 rounded-3xl hover:scale-95 transition duration-150 ease-in-out"
+        type="button"
+      >
+        Connect Wallet
+      </button>
+    </navbar>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,30 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <main>
-        <h1 className="text-3xl font-bold underline text-center">
-          Hello world!
-        </h1>
+    <div className="relative">
+      <div className="z-50 relative top-0">
+        <Navbar />
+      </div>
+      <main className="flex min-h-screen justify-center items-center text-center overflow-hidden relative">
+        <div>
+          <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-sky-600 to-white">
+            Welcome to Split-A-Bill!
+          </h1>
+          <h3 className="py-4 text-2xl">
+            We help automate calculate bill splitting for the crypto
+            enthrusiasts!
+          </h3>
+          <button
+            className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 px-4 py-2 rounded-full hover:scale-95 transition duration-150 ease-in-out"
+            type="button"
+          >
+            Get Started Now
+          </button>
+        </div>
       </main>
     </div>
   );
