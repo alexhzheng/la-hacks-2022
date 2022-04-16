c98be2a18adf42d4ca0806fdced09825ec154b77
Brandon Wang <13385547+brandonwang1@users.noreply.github.com>
2022-04-16 18:29:15 -0400

    ankr

---
+++ api/ankr.ts
@@ -0,0 +1,45 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import { ethers } from "ethers";
+
+const rpcUrl = "https://rpc.ankr.com/eth_rinkeby";
+const testWallet = "0x86283791B4e9BF64AA71b921A302559b48911c61";
+
+/* Test the ethers and thirdweb sdk */
+async function testRPC() {
+
+  const provider = new ethers.providers.JsonRpcProvider(rpcUrl);
+  const etherscanAPIKey = "R4THFBMEYI8YEYQ31D5H2VBAUMTGMZ7TVZ";
+  const etherscanProvider = new ethers.providers.EtherscanProvider("rinkeby", etherscanAPIKey);
+
+  const sdk = new ThirdwebSDK(rpcUrl);
+  const signer = provider.getSigner()
+
+  const balance = await provider.getBalance(testWallet)
+  console.log("Balance in ether: ", ethers.utils.formatEther(balance))
+
+  console.log(await getRecentTransactions(etherscanProvider, testWallet));
+}
+
+/*
+Returns the recent transactions of an address. From https://github.com/ethers-io/ethers.js/issues/326
+*/
+const getRecentTransactions = async (provider: any, address: string) => {
+  const currentBlock = await provider.getBlockNumber()
+  const blockTime = 15; // ETH block time is 15 seconds
+
+  //Block number 2 hours, 24 hours and 48 hours ago
+  const block2 = currentBlock - (2 * 60 * 60 / blockTime);
+  const block24 = currentBlock - (24 * 60 * 60 / blockTime);
+  const block48 = currentBlock - (48 * 60 * 60 / blockTime);
+
+  // Get all txs for address since 2 hours ago
+  let history = await provider.getHistory(address, block2, currentBlock);
+
+  // If you got nothing back (i.e no txns), try 24 hours and then 48 hours
+  (history.length === 0 ? history = await provider.getHistory(address, block24, currentBlock) : null);
+  (history.length === 0 ? history = await provider.getHistory(address, block48, currentBlock) : null);
+
+  return history;
+}
+
+export default testRPC;
